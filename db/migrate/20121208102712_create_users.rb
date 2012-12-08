ad5d307aad4170d3550ddc1d81ce2141b8a7d306
Richard Michael <rmichael@edgeofthenet.org>
2012-12-08 16:31:52 +0100

    Restore the reversible migration, use standard add_index, but custom column type. Normal schema file format.

--- db/migrate/20121208102712_create_users.rb
+++ db/migrate/20121208102712_create_users.rb
@@ -1,19 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
-  def up
+  def change
     create_table :users do |t|
       t.string :name
-      t.string :email, null: false
+      t.column :email, 'citext', null: false
 
       t.timestamps
     end
 
-    execute 'CREATE UNIQUE INDEX users_email_lowercase ON users ( lower(email) );'
-  end
-
-  def down
-    # Note: "DROP TABLE users;" will drop any indices on that table as well.
-    #       With separate migrations for index creation, use:
-    #         "execute 'DROP INDEX users_email_lowercase;'".
-    drop_table :users
+    add_index :users, :email, unique: true
   end
 end
