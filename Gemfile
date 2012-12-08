a848514ad6c27189f38e0de98054f007df8e69cb
Richard Michael <rmichael@edgeofthenet.org>
2012-12-08 14:40:17 +0100

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source :rubygems
+
+gem 'jquery-rails'
+gem 'rails'
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyracer', :platforms => :ruby
+
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaseInsensitiveEmail::Application.config.secret_token = '473c397b95a077606d53e9bb5814be51979bc4339aa4356dd7b0907d22ea23695410927cb314851518c19670c8c1beb7358b2e67325f06dd41586262c0b49a25'
